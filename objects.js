eac79022af2ddd2396b1439151aa0413691799a2
Luminator1122 <next13.gen.coder@gmail.com>
2023-09-08 08:54:03 +0500

    started work on Objects concepts

---
+++ objects.js
@@ -0,0 +1,52 @@
+"use strict";
+var Car = [{
+        company: "Honda",
+        model: "VXL Auto",
+        Color: "White",
+        brand: "GLI",
+        engine: "1300CC",
+        Chesis: "ANBG45000653325",
+        gears: "Five+one reverse",
+        headlights: "Beam Lights ",
+        doors: "04-door, Auto close",
+        entry: "Key less entry",
+        drive: "smooth drive",
+        year: "1986"
+    },
+    {
+        company: "TOYOTA",
+        model: "VXL Auto",
+        Color: "White",
+        brand: "GLI Auto",
+        engine: "1300-CC",
+        Chesis: "AHNBG-00653325",
+        gears: "Four + one reverse",
+        headlights: "Beam Lights ",
+        doors: "02-door, Auto close",
+        entry: "Key  entry",
+        drive: "Fast drive",
+        year: "1998"
+    },
+    {
+        company: "KIA",
+        model: "VXL Auto",
+        Color: "White",
+        brand: "BVR-DMD-Auto",
+        engine: "1300CC",
+        Chesis: "ANBG45000653325",
+        gears: "Five+one reverse",
+        headlights: "Beam Lights ",
+        doors: "04-door, Auto close",
+        entry: "Key less entry",
+        drive: "smooth drive",
+        year: "1923"
+    }
+];
+// console.log("Car", Car)
+// console.log("*******")
+// console.log("Car", Car.Color, Car.company, Car.Chesis)
+console.log("Cars", Car[1].year);
+console.log("Cars", Car[1].Color);
+console.log("Cars", Car[1].engine);
+console.log("Cars", Car[1].drive);
+console.log("Cars", Car);
